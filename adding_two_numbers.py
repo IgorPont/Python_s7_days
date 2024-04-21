8b579d5fa3f5b999c9351e1c7f494a666fa92ca5
igor_pont <pontigor11@gmail.com>
2024-04-21 20:42:43 +0300

    Программа сложения двух чисел готова

---
+++ adding_two_numbers.py
@@ -0,0 +1,9 @@
+def sum(num1: int, num2: int) -> int:
+	return num1 + num2
+
+
+if __name__ == "__main__":
+	print('Программа сложения двух чисел')
+	num1 = int(input('Введите первое число: '))
+	num2 = int(input('Введите второе число: '))
+	print('Cумма введенных чисел: ', sum(num1, num2))
