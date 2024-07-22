c591eb8f45dfd7e05289ac2eb8323ea98660525d
igor_pont <pontigor11@gmail.com>
2024-07-23 01:08:17 +0300

    Исправил ошибки

--- function.py
+++ function.py
@@ -1,11 +1,11 @@
 def function(x: int, y: int, z: int) -> int:
-	x = x
-	y = y
-	z = z
-	res = x^2 * (2 * y + 5 * z)
-	return res
+    x = x
+    y = y
+    z = z
+    res = x ^ 2 * (2 * y + 5 * z)
+    return res
 
 
 if __name__ == "__main__":
-	x, y, z = map(int, input("Введите три числа через пробел: ").split())
-	print(f'Результат вычисления: x^2 (2y + 5z) = {function(x, y, z)}')
+    x, y, z = map(int, input("Введите три числа через пробел: ").split())
+    print(f'Результат вычисления: x^2 (2y + 5z) = {function(x, y, z)}')
